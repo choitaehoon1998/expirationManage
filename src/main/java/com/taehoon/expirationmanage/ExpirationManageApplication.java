053d594caea4089e4cf5d718104075a6d89201f4
choitaehoon1998 <hugebigdream@gmail.com>
2021-07-26 21:26:07 +0900

    Default Project Setting

---
+++ src/main/java/com/taehoon/expirationmanage/ExpirationManageApplication.java
@@ -0,0 +1,13 @@
+package com.taehoon.expirationmanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpirationManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExpirationManageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/taehoon/expirationmanage/ExpirationManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taehoon.expirationmanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpirationManageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
